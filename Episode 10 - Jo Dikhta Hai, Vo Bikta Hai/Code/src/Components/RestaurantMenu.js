692923ed7228e8a0a3aeb765dfb52c84bc5b74a9
Sane Aalam <saneraza78692@gmail.com>
2025-06-22 23:54:03 +0530

    update: restaurantMenu component

--- Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/Code/src/Components/RestaurantMenu.js
+++ Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/Code/src/Components/RestaurantMenu.js
@@ -1,59 +1,90 @@
+import { CDN_URL } from "../Utils/CommonFile.js";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../Utils/useRestaurantMenu.js";
 import SimmerEffect from "./SimmerEffect";
-import React from "react";
-import useRestaurantMenu from "../Utils/useRestaurantMenu";
 
-// *documents
-// As you know, we are using clinet-side-routing
-// backend -> req.query.parameter used
-// here we need to used useParams();
-
-const RestaurantMenu = () => {
+const RestaurantMenuCard = () => {
   const { resId } = useParams();
 
-  //! custom hook:
-  // each component is focused on a specific task
-  const restaurantInfo = useRestaurantMenu(resId);
+  const resInfo = useRestaurantMenu(resId);
 
-  if (restaurantInfo === null) {
-    return <SimmerEffect />;
-  }
+  if (resInfo === null) return <SimmerEffect />;
 
-  // object understanding of fetech data
-  const { id, name, city, costForTwo, cuisines, avgRating, sla } =
-    restaurantInfo?.cards[2]?.card?.card?.info;
+  const {
+    name,
+    cuisines,
+    costForTwoMessage,
+    totalRatingsString,
+    avgRatingString,
+    areaName,
+    sla,
+    feeDetails,
+  } = resInfo?.cards[2]?.card?.card?.info;
 
-  const cards =
-    restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
-
-  let itemCards =
-    cards.find((fullcard) => fullcard?.card?.card?.itemCards)?.card?.card
-      ?.itemCards || [];
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  console.log(itemCards);
 
   return (
-    <div className="restaurant-menu">
-      <h1 className="restaurant-title">RestaurantMenu: {name}</h1>
-      <br />
-      <ul>
-        <p>id : {id}</p>
-        <p>city : {city}</p>
-        <p>costforTwo : {costForTwo / 100}</p>
-        <p>avgRating : {avgRating}</p>
-        <p>deliveryTime : {sla.deliveryTime}</p>
-        <p>cuisines : {cuisines.join(", ")}</p>
-        <br />
-      </ul>
-      <h1 className="restaurant-title">Menu: </h1>
-      <br />
-      <ol>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - Rs/- {item.card.info.defaultPrice / 100}
-          </li>
+    <div className="menu">
+      <div className="title1">
+        Home/India/{resInfo?.cards[0]?.card?.card.text}
+      </div>
+      <h1 className="heading11">{name}</h1>
+      <hr />
+      <div className="menucard">
+        <div>
+          <div className="rating">
+            <span className="span">★</span> &nbsp;
+            {avgRatingString}({totalRatingsString})&nbsp; • &nbsp;
+            {costForTwoMessage}
+          </div>
+        </div>
+        <div className="cuisines">{cuisines}</div>
+        <div className="outlet">
+          <div className="sc-lizKOf bBYPYZ">
+            <div className="sc-fsvrbR jnMRZP"></div>
+            <div className="sc-jOnpCo kEBcrx"></div>
+            <div className="sc-fsvrbR jnMRZP"></div>
+          </div>
+          <div className="wewqdiok">
+            Outlet
+            <span className="card1">&nbsp; &nbsp;{areaName}</span>▾
+            <div>{sla?.slaString}</div>
+          </div>
+        </div>
+        <hr />
+        <div className="msg">
+          <img
+            width="20px"
+            height="20px"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_40,h_40/v1648635511/Delivery_fee_new_cjxumu"
+          />
+          {feeDetails?.message.replace("<b>2.3 kms</b>", "2.3 km ")}
+        </div>
+      </div>
+      <div className="boxgray"></div>
+      <div>Recommanded (20)</div>
+      <div>
+        {itemCards.map((res) => (
+          <div className="newmenuinfo" key={res?.card?.info?.id}>
+            <div className="menucont">
+              <div className="menutile">{res?.card?.info?.name}</div>
+              <div>
+                ₹&nbsp;{
+                  res?.card?.info?.defaultPrice / 100
+                  // res?.card?.info?.variantsV2?.pricingModels?.price / 100
+                }
+              </div>
+              <div className="menudesc">{res?.card?.info?.description}</div>
+            </div>
+            <img width="156px" className="imgmenu" src={CDN_URL + res?.card?.info?.imageId}></img>
+            <hr />
+          </div>
         ))}
-      </ol>
+      </div>
     </div>
   );
 };
 
-export default RestaurantMenu;
+export default RestaurantMenuCard;
